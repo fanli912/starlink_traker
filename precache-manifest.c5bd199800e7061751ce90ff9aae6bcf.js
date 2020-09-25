afbf8bb198ad1571248af2d5c1ed0d9ff753728e
fanli912 <lifan@usc.edu>
2020-09-24 23:03:24 -0700

    Updates

---
+++ precache-manifest.c5bd199800e7061751ce90ff9aae6bcf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a679ad58b2f9a773fc2106c77140d180",
+    "url": "/starlink_traker/index.html"
+  },
+  {
+    "revision": "e76e16bd0827b78f897b",
+    "url": "/starlink_traker/static/css/2.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "7a41bcd18edb808943eb",
+    "url": "/starlink_traker/static/css/main.5215f6ad.chunk.css"
+  },
+  {
+    "revision": "e76e16bd0827b78f897b",
+    "url": "/starlink_traker/static/js/2.b82c062d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/starlink_traker/static/js/2.b82c062d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a41bcd18edb808943eb",
+    "url": "/starlink_traker/static/js/main.e85cd61d.chunk.js"
+  },
+  {
+    "revision": "d98a72431f14a808b86b",
+    "url": "/starlink_traker/static/js/runtime-main.9a0cc89a.js"
+  },
+  {
+    "revision": "141ccf71d7c4a352123d5d70c62a6d96",
+    "url": "/starlink_traker/static/media/Satellite.141ccf71.svg"
+  }
+]);
